5ee5ddb4f817570d10b18066f74bd9834f77f7ee
adamali90 <ukash.upwork@gmail.com>
2022-12-21 01:28:22 +0300

    twilio add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -644,7 +644,7 @@ work easily with a team. Improving the tech world is my passion..</p>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="//code.tidio.co/ywx2n8wuonibcm7kxfetbeiq4kcbn888.js" async></script>
+  
   <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
   <script src="assets/vendor/aos/aos.js"></script>
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
